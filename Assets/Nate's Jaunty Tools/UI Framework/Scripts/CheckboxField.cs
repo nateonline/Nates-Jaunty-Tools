74d9bd631c2167ec4f97a938228e5cf926296dfc
AgentIsaacson <agentisaacson@gmail.com>
2021-10-01 11:19:48 -0600

    finished all UI Framework stuff

--- Assets/Nate's Jaunty Tools/UI Framework/Scripts/CheckboxField.cs
+++ Assets/Nate's Jaunty Tools/UI Framework/Scripts/CheckboxField.cs
@@ -4,26 +4,74 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using TMPro;
+using NatesJauntyTools;
 
-public enum DisplayType { Unselected, Selected, MixedSelection }
+public enum SelectionType { Unselected, Selected, Mixed }
 
 public class CheckboxField : InputField
 {
-	[SerializeField] protected TMP_Dropdown checkbox;
-	[SerializeField] List<string> defaultOptions = new List<string>();
-	public System.Type optionType = null;
-	public List<object> options = new List<object>();
+	[SerializeField] protected Button button;
+	[SerializeField] protected Image image;
+	[SerializeField] protected Sprite falseIcon;
+	[SerializeField] protected Sprite trueIcon;
+	[SerializeField] protected Sprite mixedIcon;
+	[SerializeField] SelectionType selectionValue; public SelectionType SelectionValue
+	{
+		get { return selectionValue; }
+		set { selectionValue = value; UpdateSprite(); }
+	}
 
-	public UnityEvent<string> OnSubmit;
+	public UnityEvent<SelectionType> OnSubmit;
 
 	public override string Value
 	{
-		get { return options[checkbox.value].ToString(); }
-		set { checkbox.value = options.IndexOf(value); }
+		get { return selectionValue.ToString(); }
+		set
+		{
+			if (SelectionType.TryParse(value, true, out SelectionType parsedSelectionType)) {SelectionValue = parsedSelectionType;}
+			else { Debug.LogWarning($"CheckboxField didn't understand \"{value}\" when trying to set its value"); }
+		}
+	}
+
+	void OnValidate()
+	{
+		Debug.Log("OnValidate");
+		
+		SelectionValue = selectionValue;
 	}
 
 	public override void Submit()
 	{
-		OnSubmit.Invoke(Value);
+		OnSubmit.Invoke(SelectionValue);
+	}
+
+	public void OnButtonClick()
+	{
+		switch (SelectionValue)
+		{
+			case SelectionType.Unselected:
+			case SelectionType.Mixed:
+				SelectionValue = SelectionType.Selected;
+				break;
+
+			case SelectionType.Selected:
+				SelectionValue = SelectionType.Unselected;
+				break;
+		}
+	}
+
+	void UpdateSprite()
+	{
+		Sprite iconToDisplay = null;
+
+		switch (selectionValue)
+		{
+			case SelectionType.Unselected: iconToDisplay = falseIcon; break;
+			case SelectionType.Selected: iconToDisplay = trueIcon; break;
+			case SelectionType.Mixed: iconToDisplay = mixedIcon; break;
+		}
+
+		image.enabled = (iconToDisplay != null);
+		image.sprite = iconToDisplay;
 	}
 }
